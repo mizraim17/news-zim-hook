069b38d9e1a72a9b8b404a8538f284e34ef819a8
kaneki <mizraim.martinez@gmail.com>
2019-08-27 20:19:49 -0500

    add library materalize

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import New from "./Component/New";
+import { Button } from "react-materialize";
 
 function App() {
   return (
     <div className="App">
-      <h1>news </h1>
+      <h1>landingPage news </h1>
 
       <New></New>
+
+      <Button>button</Button>
     </div>
   );
 }
