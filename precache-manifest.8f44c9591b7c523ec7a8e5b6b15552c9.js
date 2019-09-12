de72e0945464027a4be3aff3fc530bee856eef45
kaneki <mizraim.martinez@gmail.com>
2019-09-12 02:07:06 -0500

    Updates

---
+++ precache-manifest.8f44c9591b7c523ec7a8e5b6b15552c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43401b723f8031e7fdffd14ed69cc7ba",
+    "url": "/news-zim-hook/index.html"
+  },
+  {
+    "revision": "6e85f7915aa53abc4490",
+    "url": "/news-zim-hook/static/css/main.8d021aef.chunk.css"
+  },
+  {
+    "revision": "ad400ad4cbead250e8bb",
+    "url": "/news-zim-hook/static/js/2.44f9d7e5.chunk.js"
+  },
+  {
+    "revision": "6e85f7915aa53abc4490",
+    "url": "/news-zim-hook/static/js/main.12a8fd98.chunk.js"
+  },
+  {
+    "revision": "b77cbe93da445117d502",
+    "url": "/news-zim-hook/static/js/runtime~main.3661c013.js"
+  }
+]);
