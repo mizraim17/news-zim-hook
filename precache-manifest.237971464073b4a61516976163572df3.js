31ef71a3a51bb77936a57a1130a7ed1d586c58b1
kaneki <mizraim.martinez@gmail.com>
2020-06-08 02:12:09 -0500

    Updates

---
+++ precache-manifest.237971464073b4a61516976163572df3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9287d7908618dafc24725d4d92336c",
+    "url": "/news-zim-hook/index.html"
+  },
+  {
+    "revision": "678ad8f094ae66147a9b",
+    "url": "/news-zim-hook/static/css/main.bfd8aa4a.chunk.css"
+  },
+  {
+    "revision": "8e9db1f0b642200e6cc7",
+    "url": "/news-zim-hook/static/js/2.778abb40.chunk.js"
+  },
+  {
+    "revision": "678ad8f094ae66147a9b",
+    "url": "/news-zim-hook/static/js/main.3a6360aa.chunk.js"
+  },
+  {
+    "revision": "b77cbe93da445117d502",
+    "url": "/news-zim-hook/static/js/runtime~main.3661c013.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/news-zim-hook/precache-manifest.ab7b8ca35ac45247ab4a7fe0d0e85e57.js"
+  "/news-zim-hook/precache-manifest.237971464073b4a61516976163572df3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/news-zim-hook/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
